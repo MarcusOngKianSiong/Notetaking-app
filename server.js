efcc47fc17af407ce5842a9c796c0dfd75de4b93
Marcus <marcusongkiansiong@gmail.com>
2022-08-06 12:26:04 +0800

    adding port for heroku to solve the problem relating to accessing app through heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
@@ -7,6 +8,6 @@ app.get('/',(req,res)=>{
     res.render('index.ejs')
 })
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Listening to port 3000")
 })
