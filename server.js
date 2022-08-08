99034eb944469ae1abd3af535c5af4b535219b44
Marcus <marcusongkiansiong@gmail.com>
2022-08-08 15:55:08 +0800

    testing database connection

--- server.js
+++ server.js
@@ -8,12 +8,22 @@ const PORT = process.env.PORT || 3000;
 const db = mongoose.connection;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `account`;
 mongoose.connect(MONGODB_URI);
+const authenticationSchema = {username: String,password: String};
 db.on('open' , ()=>{});
+const account = mongoose.model('account',authenticationSchema);
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
+    console.log("------------LOGIN PAGE OPENING!!!--------"); 
+    db.find((err,res)=>{
+        if(err){
+            console.log("----ERROR------")
+        }else{
+            console.log(res);
+        }
+    })
     res.render('index.ejs',{
         loginFailed: false
     })
