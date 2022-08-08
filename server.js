3b25a7bd7853fd02c5feea4eea8ed064b2126b8c
Marcus <marcusongkiansiong@gmail.com>
2022-08-08 15:21:21 +0800

    added database connection to mongodb atlas

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express()
+const mongoose = require("mongoose");
+
 const PORT = process.env.PORT || 3000;
 
+// Database connection
+const db = mongoose.connection;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `account`;
+mongoose.connect(MONGODB_URI);
+db.on('open' , ()=>{}); 
+
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:false}));
 
 app.get('/',(req,res)=>{
-    res.render('index.ejs')
+    res.render('index.ejs',{
+        loginFailed: false
+    })
+})
+
+app.post('/login',(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    res.render('index.ejs',{
+        loginFailed: true
+    })
 })
 
 app.listen(PORT,()=>{
