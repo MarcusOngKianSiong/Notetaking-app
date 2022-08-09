07f15e8950f54955be6199860fb5706935122103
Marcus <marcusongkiansiong@gmail.com>
2022-08-09 12:05:52 +0800

    testing authentication

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const express = require('express')
 const app = express()
 const mongoose = require("mongoose");
+const authenticate = require("../controller/authentication.js")
 
 const PORT = process.env.PORT || 3000;
 
-// Database connection
+// ESTABLISHING DATABASE CONNECTION
 const db = mongoose.connection;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `account`;
 mongoose.connect(MONGODB_URI);
 const authenticationSchema = {username: String,password: String};
 db.on('open' , ()=>{});
 const account = mongoose.model('accounts',authenticationSchema);
-account.create({username:"timmy",password:"123"},(error,creatingAcc)=>{
-    if(error){
-        console.log(error);
-    }else{
-        console.log("ACCOUNT CREATED: ",creatingAcc);
-    }
-})
+// account.create({username:"timmy",password:"123"},(error,creatingAcc)=>{
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log("ACCOUNT CREATED: ",creatingAcc);
+//     }
+// })
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:false}));
+app.use('/login',authenticate);                                     // LOGIN STEP
 
 app.get('/',(req,res)=>{
     console.log("------------LOGIN PAGE OPENING!!!--------"); 
-
     account.find((err,res)=>{
         if(err){
             console.log("----ERROR------")
@@ -37,14 +38,9 @@ app.get('/',(req,res)=>{
     })
 })
 
-app.post('/login',(req,res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
-    res.render('index.ejs',{
-        loginFailed: true
-    })
-})
+app.post('/login',authenticate)
 
 app.listen(PORT,()=>{
     console.log("Listening to port 3000")
 })
+
