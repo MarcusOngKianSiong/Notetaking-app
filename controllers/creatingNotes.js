fe21cf7721051f643d6c06f21c147c43764f44ad
Marcus <marcusongkiansiong@gmail.com>
2022-08-09 14:20:11 +0800

    next step

--- controllers/creatingNotes.js
+++ controllers/creatingNotes.js
@@ -6,12 +6,12 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/' + `account
 mongoose.connect(MONGODB_URI);
 const db = mongoose.connection;
 const authenticationSchema = {username: String,password: String};
-const account = mongoose.model('accounts',authenticationSchema);
+// const account = mongoose.model('accounts',authenticationSchema);
 db.on('open' , ()=>{}); 
 
 creatingNewNote.post("/createNewNote",(req,res)=>{
     const collections = db.collections();
-    res.render("note.ejs",{
+    res.render("note.ejs",{ 
         collections: collections
     })
 })
