c8afd78b89f3bcbcfda7f1794956039d016376a0
Marcus <marcusongkiansiong@gmail.com>
2022-08-09 12:26:28 +0800

    testing authenticaton feature

---
+++ controllers/authentication.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const authenticate = express.Router();
+
+// database access setup
+const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `account`;
+mongoose.connect(MONGODB_URI);
+const db = mongoose.connection;
+const authenticationSchema = {username: String,password: String};
+const account = mongoose.model('accounts',authenticationSchema);
+db.on('open' , ()=>{}); 
+
+authenticate.post('/login',(req,res)=>{
+    // Data from user
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if username exists
+    account.find({username: username,password: password},(error,findings)=>{
+        if(error){
+            res.render('../views/test.ejs',{
+                authenticationOutcome: "Something is wrong!!!!"
+            });
+        }else{
+            res.render('../views/test.ejs',{
+                authenticationOutcome: findings
+            });
+        }
+        res.render()
+    })
+})
+
+// res.render('index.ejs',{
+//     loginFailed: true
+// })
+
+module.exports = authenticate;
