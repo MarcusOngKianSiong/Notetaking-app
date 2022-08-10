40f5d295984869e2f52d2020fbb97de3fbeeb716
Marcus <marcusongkiansiong@gmail.com>
2022-08-10 14:41:42 +0800

    next step

--- controllers/creatingNotes.js
+++ controllers/creatingNotes.js
@@ -5,7 +5,7 @@ const creatingNewNote = express.Router();
 const accountsData = require("../public/database/accessPoint.js").accountsData;
 const notesData = require("../public/database/accessPoint.js").notesData;
 
-creatingNewNote.get("/createNewNote",(req,res)=>{
+creatingNewNote.post("/createNewNote",(req,res)=>{
     console.log("-----CREATING NEW NOTE-----")
     // create a document in the collection
     notesData.create({user: "marcus", name: "random", identificationNumber: "1", dependencies: "0", contents: "Something here"})
