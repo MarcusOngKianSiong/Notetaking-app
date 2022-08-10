9048ae10ad121e9e571f0e324154b088abd31155
Marcus <marcusongkiansiong@gmail.com>
2022-08-10 15:45:08 +0800

    next step

--- controllers/creatingNotes.js
+++ controllers/creatingNotes.js
@@ -18,7 +18,9 @@ creatingNewNote.post('/createNewNote',(req,res)=>{
     console.log("-----CREATING NEW NOTE-----")
     // create a document in the collection
     // Gather a list of documents by name
-    notesData.create({user: "marcus", name: "random", identificationNumber: "1", dependencies: "0", contents: "Something here"})
+    setTimeout(function(){
+        notesData.create({user: "marcus", name: "random", identificationNumber: "1", dependencies: "0", contents: "Something here"})
+    }, 1000);
     notesData.find({user: "marcus"},(error,notes)=>{
         res.render('note.ejs',{
             notes: notes
