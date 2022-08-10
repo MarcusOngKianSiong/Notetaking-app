21722f25374518d497109ad9a1dbabcf35c46c86
Marcus <marcusongkiansiong@gmail.com>
2022-08-10 13:50:44 +0800

    next step

--- controllers/creatingNotes.js
+++ controllers/creatingNotes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const creatingNewNote = express.Router();
 
-db.on('open' , ()=>{});
-
 creatingNewNote.get("/createNewNote",(req,res)=>{
     res.render('note.ejs')
 })
