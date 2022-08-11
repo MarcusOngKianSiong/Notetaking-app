fedc0046159c9e0439db17028fb0990d38a90d2c
Marcus <marcusongkiansiong@gmail.com>
2022-08-11 16:16:45 +0800

    next step

--- models/retrievingData.js
+++ models/retrievingData.js
@@ -3,7 +3,7 @@ const notesData = require("../public/database/accessPoint.js").notesData;
 
 function findFirstDocumentIdentificationNumber(){
     console.log("----FINDING FIRST DOCUMENT IDENTIFICATION NUMBER----")
-    console.log(notesData.findOne())
+    console.log(notesData.findOne().identificationNumber)
     return notesData.findOne().identificationNumber;
 }
 
