024168d5400ea97924b57b8a926b99abd6d2abb0
Marcus <marcusongkiansiong@gmail.com>
2022-08-11 16:12:47 +0800

    next step

--- models/retrievingData.js
+++ models/retrievingData.js
@@ -2,6 +2,7 @@ const accountsData = require("../public/database/accessPoint.js").accountsData;
 const notesData = require("../public/database/accessPoint.js").notesData;
 
 function findFirstDocumentIdentificationNumber(){
+    console.log("----FINDING FIRST DOCUMENT IDENTIFICATION NUMBER----")
     return notesData.findOne().identificationNumber;
 }
 
