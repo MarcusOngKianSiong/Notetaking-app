1a9145010862007a7dcaf9d2555296f98c52d30e
Marcus <marcusongkiansiong@gmail.com>
2022-08-11 16:27:09 +0800

    next step

--- models/retrievingData.js
+++ models/retrievingData.js
@@ -4,7 +4,7 @@ const notesData = require("../public/database/accessPoint.js").notesData;
 function findFirstDocumentIdentificationNumber(){
     console.log("----FINDING FIRST DOCUMENT IDENTIFICATION NUMBER----")
     console.log(notesData.findOne().identificationNumber)
-    return notesData.findOne().identificationNumber;
+    return notesData.find()[0].identificationNumber;
 }
 
 module.exports = findFirstDocumentIdentificationNumber
