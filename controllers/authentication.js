4d2966cec1ef215665d8ada6b3b6f63ca079330e
Marcus <marcusongkiansiong@gmail.com>
2022-08-11 16:55:11 +0800

    next step

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -23,7 +23,6 @@ authenticate.post('/login',(req,res)=>{
                 });
             }else{
                 notesData.find({username: "marcus"},(error,notes)=>{
-                    console.log("HERE: ",findFirst());
                     notesData.find((error,notes)=>{
                         res.render('../views/note.ejs',{
                             notes: notes,
