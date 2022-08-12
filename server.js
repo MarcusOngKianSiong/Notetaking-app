d0016eca28637e879538b2580580155a123f0ac7
Marcus <marcusongkiansiong@gmail.com>
2022-08-12 18:19:25 +0800

    next step

--- server.js
+++ server.js
@@ -8,21 +8,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection',(socket)=>{
-    console.log('a user is connected...');
-    socket.on('changes',(content)=>{
-        console.log("content: ",content);
-    })
-})
-
-
-
 // Controllers
 const authenticate = require("./controllers/authentication.js")
 const createNewNote = require("./controllers/creatingNotes.js");
-const { Socket } = require('dgram');
-
-
 
 // model
 app.post('/createNewNote',createNewNote);
